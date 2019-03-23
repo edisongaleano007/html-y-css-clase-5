78407b615394df932c11b38cbd38b83126b7da8d
Rutan Vivelab <rutanvivelab@68.0.17.175.adsl-pool.jlccptt.net.cn>
2019-03-23 13:15:09 -0500

    change name el script

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
+// web api
 alert("cuidado");
+const item1 = document.createElement("LI")
+item1.textContent = "teaching on team international top gun"
+// document.queryselector
+const lista = document.querySelector(" la lista a ingectar")
+
+// destino appendchild (nodo agragar)
+document.body.appendChild(item1)
+
+//segundo elemento
+const item2 = document.createElement("LI")
+item2.textContent = "segundo intento"
+document.body.appendChild(item2)
